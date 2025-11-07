7bb9a2cb5464ef66f3cf1d50ce46ae994c06643c
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 10:50:37 +0700

    Menambahkan fungsi Composable TampilData dengan parameter onGoFormulir dan onGoSplash untuk navigasi antar layar

--- app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
@@ -1,2 +1,13 @@
 package com.example.tugasnavigasi.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+    onGoFormulir: () -> Unit,   // ke Formulirku
+    onGoSplash: () -> Unit      // ke Splash (Beranda)
+){
+
+}
