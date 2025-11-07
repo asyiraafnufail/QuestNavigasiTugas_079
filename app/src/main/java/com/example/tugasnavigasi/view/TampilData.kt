c81879a2d52844e5e2894b37b2da326ae641746f
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 10:54:00 +0700

    Menambahkan scaffold

--- app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
@@ -1,7 +1,13 @@
 package com.example.tugasnavigasi.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.tugasnavigasi.R
 
@@ -16,4 +22,21 @@ fun TampilData(
         Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
         Pair(stringResource(id = R.string.aLamat), "Yogyakarta")
     )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.tampil),
+                        color = Color.White
+                    )
+                },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    ){
+
+    }
 }
