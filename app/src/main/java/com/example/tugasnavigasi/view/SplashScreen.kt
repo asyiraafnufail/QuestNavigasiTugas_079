dfddb8a595f2afebcaf47039f74a0cee67ca614f
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 10:40:46 +0700

    Menambahkan Column dengan pengaturan ukuran penuh, penyusunan elemen di tengah secara horizontal dan vertikal

--- app/src/main/java/com/example/tugasnavigasi/view/SplashScreen.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/SplashScreen.kt
@@ -1,10 +1,13 @@
 package com.example.tugasnavigasi.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -29,5 +32,14 @@ fun SplashScreen(
             .fillMaxSize()
             .background(bg)
             .padding(horizontal = 24.dp)
-    )
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+
+        }
+    }
 }
