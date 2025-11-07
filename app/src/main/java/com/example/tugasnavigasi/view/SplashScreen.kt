582cf0cd75449569a86ca3d407ffb431179d1ce0
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 10:39:42 +0700

    Menambahkan Box dengan pengaturan ukuran penuh, latar belakang, dan padding horizontal

--- app/src/main/java/com/example/tugasnavigasi/view/SplashScreen.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/SplashScreen.kt
@@ -1,8 +1,13 @@
 package com.example.tugasnavigasi.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 data class SplashUser(
     val name: String = "Asyiraaf Nufail",
@@ -18,4 +23,11 @@ fun SplashScreen(
     val bg = Color(0xFFF1ECFF)          // ungu sangat muda
     val primary = Color(0xFF5B3DF5)     // ungu tombol/aksen
     val textDark = Color(0xFF2E2A47)
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(bg)
+            .padding(horizontal = 24.dp)
+    )
 }
