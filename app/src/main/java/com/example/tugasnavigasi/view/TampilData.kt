0c31b0756196ca623d6b756b2a8ecd33969048fb
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 10:55:33 +0700

    Menambahkan column

--- app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import com.example.tugasnavigasi.R
 
@@ -49,7 +50,12 @@ fun TampilData(
                 .fillMaxSize(),
             verticalArrangement = Arrangement.SpaceBetween
         ){
+            Column(
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+            ){
 
+            }
         }
     }
 }
