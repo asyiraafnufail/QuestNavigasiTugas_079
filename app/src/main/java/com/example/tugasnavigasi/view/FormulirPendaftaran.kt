8b87220ffaf7b8edc12334ae0cf2619555305959
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 11:00:06 +0700

    Menambahkan berbagai macam variabel

--- app/src/main/java/com/example/tugasnavigasi/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/FormulirPendaftaran.kt
@@ -1,10 +1,27 @@
 package com.example.tugasnavigasi.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun FormulirPendaftaran(
     OnSubmitBtnClick: () -> Unit
 ){
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
+
+    var hasilNama by remember { mutableStateOf("") }
+    var hasilJK by remember { mutableStateOf("") }
+    var hasilAlamat by remember { mutableStateOf("") }
+    var hasilStatus by remember { mutableStateOf("") }
+
+    val genderList = listOf("Laki-laki", "Perempuan")
+    val statusList = listOf("Janda", "Lajang", "Duda")
+
 
 }
