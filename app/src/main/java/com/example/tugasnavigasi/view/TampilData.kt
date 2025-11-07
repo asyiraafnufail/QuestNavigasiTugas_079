44967358e5f55078afd9268a369e7ed6a243e8dd
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 10:53:23 +0700

    Menambahkan string

--- app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/TampilData.kt
@@ -2,6 +2,8 @@ package com.example.tugasnavigasi.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.example.tugasnavigasi.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,5 +11,9 @@ fun TampilData(
     onGoFormulir: () -> Unit,   // ke Formulirku
     onGoSplash: () -> Unit      // ke Splash (Beranda)
 ){
-
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama_lengkap), "Contoh Nama"),
+        Pair(stringResource(id = R.string.jenis_kelamin), "Lainnya"),
+        Pair(stringResource(id = R.string.aLamat), "Yogyakarta")
+    )
 }
