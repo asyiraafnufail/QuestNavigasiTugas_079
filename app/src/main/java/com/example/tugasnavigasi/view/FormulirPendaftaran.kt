ff99e6573f1fe0b127623e2307c5bb8d8a4e1000
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 11:00:38 +0700

    Menambahkan column

--- app/src/main/java/com/example/tugasnavigasi/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/FormulirPendaftaran.kt
@@ -1,10 +1,16 @@
 package com.example.tugasnavigasi.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormulirPendaftaran(
@@ -23,5 +29,12 @@ fun FormulirPendaftaran(
     val genderList = listOf("Laki-laki", "Perempuan")
     val statusList = listOf("Janda", "Lajang", "Duda")
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
 
+    }
 }
