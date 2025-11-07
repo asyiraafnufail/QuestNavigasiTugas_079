9745b5f421bbbd775d58a083cfee8848768b097b
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 11:02:39 +0700

    Menambahkan isi card

--- app/src/main/java/com/example/tugasnavigasi/view/FormulirPendaftaran.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/FormulirPendaftaran.kt
@@ -2,12 +2,20 @@ package com.example.tugasnavigasi.view
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -63,7 +71,81 @@ fun FormulirPendaftaran(
             colors = CardDefaults.cardColors(containerColor = Color.White),
             elevation = CardDefaults.cardElevation(6.dp)
         ){
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text("NAMA LENGKAP", fontWeight = FontWeight.Bold)
+                OutlinedTextField(
+                    value = nama,
+                    onValueChange = {
+                        nama = it
+                    },
+                    placeholder = { Text("Isian nama lengkap") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                )
+                // Jenis Kelamin
+                Text("JENIS KELAMIN", fontWeight = FontWeight.Bold)
+                genderList.forEach { item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .selectable(
+                                selected = jenisKelamin == item,
+                                onClick = { jenisKelamin = item }
+                            )
+                            .padding(vertical = 2.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = jenisKelamin == item,
+                            onClick = { jenisKelamin = item }
+                        )
+                        Text(item)
+                    }
+                }
 
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("STATUS PERKAWINAN", fontWeight = FontWeight.Bold)
+                statusList.forEach { item ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .selectable(
+                                selected = status == item,
+                                onClick = { status = item }
+                            )
+                            .padding(vertical = 2.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = status == item,
+                            onClick = { status = item }
+                        )
+                        Text(item)
+                    }
+                }
+                // Alamat
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("ALAMAT", fontWeight = FontWeight.Bold)
+                OutlinedTextField(
+                    value = alamat,
+                    onValueChange = {
+                        alamat = it
+                    },
+                    placeholder = {Text("Alamat lengkap") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 8.dp)
+                )
+                // Tombol submit
+                Button(
+                    onClick = OnSubmitBtnClick,
+                    modifier = Modifier.fillMaxWidth().height(48.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF6200EE), contentColor = Color.White)
+                ) {
+                    Text(text = "Submit")
+                }
+            }
         }
     }
 }
