9f035de46b849b28e014c772fb491d3da25a511c
asyiraafnufail <asyiraaf4@gmail.com>
2025-11-07 10:37:26 +0700

    Menambahkan fungsi Composable SplashScreen dengan parameter user dan onSubmitClick

--- app/src/main/java/com/example/tugasnavigasi/view/SplashScreen.kt
+++ app/src/main/java/com/example/tugasnavigasi/view/SplashScreen.kt
@@ -1,6 +1,16 @@
 package com.example.tugasnavigasi.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 data class SplashUser(
     val name: String = "Asyiraaf Nufail",
     val nim: String = "20230140079"
+)
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+    user: com.example.navigasi.view.SplashUser = SplashUser(),
+    onSubmitClick: () -> Unit
 )
